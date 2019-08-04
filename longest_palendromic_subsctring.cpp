0f032ecfa8ae5f337305605b07407afc5e9f0c97
tfcat <danielbri3r@gmail.com>
2019-08-04 22:08:45 +0100

    Now with less memory leaks! oof

--- longest_palendromic_subsctring.cpp
+++ longest_palendromic_subsctring.cpp
@@ -1,8 +1,7 @@
 /*
 A palindrome is a sequence of characters that reads 
 the same backwards and forwards. Given a string, s,
-find the longest palindromic substring in s.
-*/
+find the longest palindromic substring in s. */
 // stats:
 // googles: 0
 // compiles: 3
@@ -16,33 +15,27 @@ find the longest palindromic substring in s.
 #include <vector>
 #include <map>
 
-struct Pair {
-	int start;
-	int end;
-};
-
-// banana
 std::string longest_palendrome(std::string input) {
 	// scan for pairs
-	std::vector<Pair*> letter_pair_locations;
-	for(int i = 0; i < input.size(); i++) {
-		for(int j = i; j < input.size(); j++) {
+	std::vector<std::pair<size_t, size_t>> letter_pair_locations;
+	for(size_t i = 0; i < input.size(); i++) {
+		for(size_t j = i; j < input.size(); j++) {
 			if(input[i] == input[j] && i != j)
-				letter_pair_locations.push_back(new Pair{i, j});
+				letter_pair_locations.emplace_back(i, j);
 		}
 	}
 
 	// find the palendromic pairs
-	std::vector<Pair*> palendromic_pairs;
-	for(Pair* pair : letter_pair_locations) {
+	std::vector<std::pair<size_t, size_t>*> palendromic_pairs;
+	for(auto &pair : letter_pair_locations) {
 		// while chars are the same and start/end aren't equal, step closer
-		int start = pair->start;
-		int end = pair->end;
+		size_t start = pair.first;
+		size_t end = pair.second;
 
 		while(input[start] == input[end] && start <= end) {
 			// you've got a palendrome
 			if(start == end || start + 1 == end) {
-				palendromic_pairs.push_back(pair);
+				palendromic_pairs.push_back(&pair);
 				break;
 			}
 			start += 1;
@@ -52,18 +45,18 @@ std::string longest_palendrome(std::string input) {
 	if(palendromic_pairs.size() == 0) return std::string("");
 
 	// find the furthest palendromic pair
-	Pair* largest_pair = new Pair {-1, -1};
-	for(Pair* pair : palendromic_pairs) {
-		if(pair->end - pair->start > largest_pair->end - largest_pair->start)
+	std::pair<size_t, size_t> *largest_pair = palendromic_pairs.back();
+	for(auto* pair : palendromic_pairs) {
+		if(pair->second - pair->first > largest_pair->second - largest_pair->first)
 			largest_pair = pair;
 	}
 
 	// build the string from pair and return it
-	std::string result(input.begin() + largest_pair->start, input.begin() + largest_pair->end + 1);
+	std::string result(input.begin() + largest_pair->first, input.begin() + largest_pair->second + 1);
 	return result;
 }
 
 int main() {
-	printf("%s\n", longest_palendrome("u know ive got that swagga boi").c_str());
+	printf("%s\n", longest_palendrome("million").c_str());
 	return 0;
 }
